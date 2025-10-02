b69d4b8fe6aa4131b8ab620f7b057503b7ce4fb8
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-10-02 00:54:03 -0300

    feat: entity investor

---
+++ src/main/java/com/invest/fastinvestment/entity/Investor.java
@@ -0,0 +1,101 @@
+package com.invest.fastinvestment.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_investors")
+public class Investor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID investorId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    @UpdateTimestamp
+    private Instant updateTimestamp;
+
+    public Investor(UUID investorId, String username, String email, String password, String name, Instant creationTimestamp, Instant updateTimestamp) {
+        this.investorId = investorId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.creationTimestamp = creationTimestamp;
+        this.updateTimestamp = updateTimestamp;
+    }
+
+    public UUID getInvestorId() {
+        return investorId;
+    }
+
+    public void setInvestorId(UUID investorId) {
+        this.investorId = investorId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Instant creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+
+    public Instant getUpdateTimestamp() {
+        return updateTimestamp;
+    }
+
+    public void setUpdateTimestamp(Instant updateTimestamp) {
+        this.updateTimestamp = updateTimestamp;
+    }
+}
