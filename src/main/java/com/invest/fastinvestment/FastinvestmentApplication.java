ee67da58b50f5cb70b1b8f223fcc76d7c2d888e8
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-10-01 23:26:04 -0300

    feat: first commit

---
+++ src/main/java/com/invest/fastinvestment/FastinvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.invest.fastinvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastinvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastinvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invest/fastinvestment/FastinvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invest.fastinvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastinvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
