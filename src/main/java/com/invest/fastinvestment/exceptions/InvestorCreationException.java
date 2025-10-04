139a8ca4bfc22c811323cca8042c8ad0c3c90589
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-10-04 20:28:11 -0300

    refact: exception handling on user create method

--- src/main/java/com/invest/fastinvestment/exceptions/InvestorCreationException.java
+++ src/main/java/com/invest/fastinvestment/exceptions/InvestorCreationException.java
@@ -1,6 +1,13 @@
 package com.invest.fastinvestment.exceptions;
 
-public class InvestorCreationException extends Exception {
+public class InvestorCreationException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvestorCreationException(String message) {
+        super(message);
+    }
+
     public InvestorCreationException(String message, Throwable cause) {
         super(message, cause);
     }
