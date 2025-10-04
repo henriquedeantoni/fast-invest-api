2b1e24db18eb9ada3bfc04449a7fc7eb98c66577
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-10-04 18:04:58 -0300

    feat: new class exception

---
+++ src/main/java/com/invest/fastinvestment/exceptions/InvestorCreationException.java
@@ -0,0 +1,7 @@
+package com.invest.fastinvestment.exceptions;
+
+public class InvestorCreationException extends Exception {
+    public InvestorCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
