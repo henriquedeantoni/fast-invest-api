c0a08f500cc08db287071dc3184e1ad70b4116f9
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-10-04 17:31:44 -0300

    test: unitary test for investor creation

---
+++ src/test/java/com/invest/fastinvestment/service/InvestorServiceTest.java
@@ -0,0 +1,89 @@
+package com.invest.fastinvestment.service;
+
+import com.invest.fastinvestment.controller.CreateInvestorDto;
+import com.invest.fastinvestment.entity.Investor;
+import com.invest.fastinvestment.repository.InvestorRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
+class InvestorServiceTest {
+
+    @Mock
+    private InvestorRepository investorRepository;
+
+    @InjectMocks
+    private InvestorService investorService;
+
+    @Captor
+    private ArgumentCaptor<Investor> investorArgumentCaptor;
+
+    @Nested
+    class createInvestor{
+
+        @Test
+        @DisplayName("Should create a investor user with success.")
+        void shouldCreateInvestorWithSuccess() {
+
+            //Arrange
+            var userInvestor = new Investor(
+                    UUID.randomUUID(),
+                    "felipezan",
+                    "felipezanatta@gmail.com",
+                    "82sq8wF@",
+                    "Felipe Zanatta",
+                    Instant.now(),
+                    null
+            );
+
+            doReturn(userInvestor).when(investorRepository).save(investorArgumentCaptor.capture());
+            var input = new CreateInvestorDto(
+                    "felipezan",
+                    "felipezanatta@gmail.com",
+                    "82sq8wF@",
+                    "Felipe Zanatta");
+            //Act
+            var output = investorService.createInvestor(input);
+
+            //Assert
+            assertNotNull(output);
+            var investorCaptured = investorArgumentCaptor.getValue();
+            assertEquals(input.username(), investorCaptured.getUsername());
+            assertEquals(input.email(), investorCaptured.getEmail());
+            assertEquals(input.password(), investorCaptured.getPassword());
+            assertEquals(input.name(), investorCaptured.getName());
+        }
+
+        @Test
+        @DisplayName("Should throw an exception when error occurs")
+        void shouldThrownAnExceptionWhenErrorOccurs() {
+
+            //Arrange
+
+            doThrow(new RuntimeException()).when(investorRepository).save(any());
+            var input = new CreateInvestorDto(
+                    "felipezan",
+                    "felipezanatta@gmail.com",
+                    "82sq8wF@",
+                    "Felipe Zanatta");
+            //Act & Assert
+            assertThrows(RuntimeException.class, () -> investorService.createInvestor(input));
+        }
+    }
+}
