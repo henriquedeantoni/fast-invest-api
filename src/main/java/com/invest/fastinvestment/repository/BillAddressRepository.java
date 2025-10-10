4ff5b2300f8aa562920368b0ee516f1d5742f236
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-10-10 00:46:14 -0300

    fix: repository sintaxe error

--- src/main/java/com/invest/fastinvestment/repository/BillAddressRepository.java
+++ src/main/java/com/invest/fastinvestment/repository/BillAddressRepository.java
@@ -1,8 +1,10 @@
 package com.invest.fastinvestment.repository;
 
+import com.invest.fastinvestment.entity.BillAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface BillAddressRepository extends JpaRepository<BillAddressRepository, UUID> {
+public interface BillAddressRepository extends JpaRepository<BillAddress, UUID> {
 }
+
